c750c75458654a4d6764d1ea94857a58b1fb5148
Farrza <farisseferagic@gmail.com>
2020-04-10 01:46:24 +0200

    fix(cli): Added usage manual

--- src/cli.cr
+++ src/cli.cr
@@ -1,15 +1,21 @@
 require "option_parser"
-require "uri"
 
 require "./wafalyzer"
 
 OptionParser.parse do |parser|
   parser.banner = BANNER
   parser.on "-v", "--version", "Show version" do
-    puts "version 1.0"
+    puts "version 0.1.0"
   end
   parser.on "-h", "--help", "Show help" do
     puts parser
+    puts "\n"
+    puts "    Starting analysis:"
+    puts "    wafalyzer https://www.target1.com https://www.target2.com"
+    puts "\n"
+    puts "    NOTES:"
+    puts "    - Full URL required"
+    puts "    - One or more targets separated by whitespace"
     exit
   end
   parser.unknown_args do |targets, b|
