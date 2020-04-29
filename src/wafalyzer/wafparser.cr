b26820187a0cd09c9911c564ba1c0b575729c59f
Faris Seferagic <farisseferagic@gmail.com>
2020-04-29 17:12:30 +0200

    fix(wafalyzer): Build fix, JSON file structure validation + WafParser spec

--- src/wafalyzer/wafparser.cr
+++ src/wafalyzer/wafparser.cr
@@ -3,51 +3,99 @@ require "./waf"
 
 class WafParser
   # :TODO handle file structure validation
-  def self.parse(path : Path)
+  def self.parse_json(path : Path)
     json = File.open(path) do |file|
       JSON.parse(file)
     end
+  end
 
-    name = json["name"].as_s
+  def self.parse(path : Path)
+    json = self.parse_json(path)
 
-    manufacturer = json["manufacturer"].as_s
+    if self.valid(json)
+      name = json["name"].as_s
 
-    website = json["website"].as_s
+      manufacturer = json["manufacturer"].as_s
+
+      website = json["website"].as_s
+
+      fingerprints = json["fingerprints"].as_h
+
+      fingerprints_final = {
+        headers:  self.to_h_of_s_r(fingerprints["headers"]),
+        contents: self.to_a_of_r(fingerprints["contents"]),
+        cookies:  self.to_a_of_r(fingerprints["cookies"]),
+        statuses: self.to_a_of_i(fingerprints["statuses"]),
+        reasons:  self.to_a_of_s(fingerprints["reasons"]),
+      }
+
+      responses = json["spec"]["responses"].as_h
+
+      normal_response = {
+        headers: self.to_h_of_s_s(responses["normal"]["headers"]),
+        body:    responses["normal"]["body"].to_s,
+        status:  responses["normal"]["status"].as_i,
+        reason:  responses["normal"]["reason"].to_s,
+      }
+
+      attack_response = {
+        headers: self.to_h_of_s_s(responses["attack"]["headers"]),
+        body:    responses["attack"]["body"].to_s,
+        status:  responses["attack"]["status"].as_i,
+        reason:  responses["attack"]["reason"].to_s,
+      }
+
+      return waf = Wafalyzer::Waf.new(
+        name,
+        manufacturer,
+        website,
+        fingerprints_final,
+        self.build_response(normal_response),
+        self.build_response(attack_response)
+      )
+    else
+      puts "Invalid file format for #{path}"
+    end
+  end
+
+  def self.valid(json : JSON::Any)
+    json = json.as_h
+    return false unless json.has_key?("name")
+    return false unless json.has_key?("manufacturer")
+    return false unless json.has_key?("website")
+    return false unless json.has_key?("fingerprints")
 
     fingerprints = json["fingerprints"].as_h
+    return false unless fingerprints.has_key?("headers")
+    return false unless fingerprints.has_key?("contents")
+    return false unless fingerprints.has_key?("cookies")
+    return false unless fingerprints.has_key?("statuses")
+    return false unless fingerprints.has_key?("reasons")
 
-    fingerprints_final = {
-      headers:  self.to_h_of_s_r(fingerprints["headers"]),
-      contents: self.to_a_of_r(fingerprints["contents"]),
-      cookies:  self.to_a_of_r(fingerprints["cookies"]),
-      statuses: self.to_a_of_i(fingerprints["statuses"]),
-      reasons:  self.to_a_of_s(fingerprints["reasons"]),
-    }
+    return false unless json.has_key?("spec")
 
-    responses = json["spec"]["responses"].as_h
+    spec = json["spec"].as_h
+    return false unless spec.has_key?("responses")
 
-    normal_response = {
-      headers: self.to_h_of_s_s(responses["normal"]["headers"]),
-      body:    responses["normal"]["body"].to_s,
-      status:  responses["normal"]["status"].as_i,
-      reason:  responses["normal"]["reason"].to_s,
-    }
+    responses = spec["responses"].as_h
 
-    attack_response = {
-      headers: self.to_h_of_s_s(responses["attack"]["headers"]),
-      body:    responses["attack"]["body"].to_s,
-      status:  responses["attack"]["status"].as_i,
-      reason:  responses["attack"]["reason"].to_s,
-    }
+    return false unless responses.has_key?("normal")
 
-    yield waf = Wafalyzer::Waf.new(
-      name,
-      manufacturer,
-      website,
-      fingerprints_final,
-      self.build_response(normal_response),
-      self.build_response(attack_response)
-    )
+    normal = responses["normal"].as_h
+    return false unless normal.has_key?("headers")
+    return false unless normal.has_key?("body")
+    return false unless normal.has_key?("status")
+    return false unless normal.has_key?("reason")
+
+    return false unless responses.has_key?("attack")
+
+    attack = responses["attack"].as_h
+    return false unless attack.has_key?("headers")
+    return false unless attack.has_key?("body")
+    return false unless attack.has_key?("status")
+    return false unless attack.has_key?("reason")
+
+    true
   end
 
   def self.build_response(
